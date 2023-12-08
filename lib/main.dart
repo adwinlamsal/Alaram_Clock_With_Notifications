7a4add40bfedd431f92ca0b3517fd07642551f62
Balkrishna Lamsal <60682654+adwinlamsal@users.noreply.github.com>
2023-12-08 09:27:02 +0545

    Local Notification Added

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,28 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest_all.dart' as tz;
 
-void main() => runApp(ChangeNotifierProvider(
-      create: (contex) => alarmprovider(),
-      child: const MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: MyApp(),
-      ),
-    ));
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+tz.initializeTimeZones();
+  flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()!
+      .requestNotificationsPermission();
+  runApp(ChangeNotifierProvider(
+    create: (contex) => alarmprovider(),
+    child: const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyApp(),
+    ),
+  ));
+}
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -26,16 +40,86 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   bool value = false;
 
+
+
+
+
+FlutterLocalNotificationsPlugin ? flutterLocalNotificationsPlugin;
+
+
+Inituilize()async{
+
+
+
+var androidInitilize = new AndroidInitializationSettings('@mipmap/ic_launcher');
+   var iOSinitilize = new DarwinInitializationSettings();
+   var initilizationsSettings =
+       InitializationSettings(android: androidInitilize, iOS: iOSinitilize);
+   flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+   await flutterLocalNotificationsPlugin!.initialize(initilizationsSettings,
+       onDidReceiveNotificationResponse:onDidReceiveNotificationResponse);
+
+
+
+
+
+}
+
+void onDidReceiveNotificationResponse(NotificationResponse notificationResponse) async {
+    final String? payload = notificationResponse.payload;
+    if (notificationResponse.payload != null) {
+      debugPrint('notification payload: $payload');
+    }
+    // await Navigator.push(
+    //   context,
+    //   MaterialPageRoute<void>(builder: (context) => (payload)),
+    // );
+}
+
   @override
   void initState() {
+
+Inituilize();
     Timer.periodic(Duration(seconds: 1), (timer) {
       setState(() {});
     });
 
     super.initState();
-
     context.read<alarmprovider>().GetData();
   }
+
+  Future notificationSelected(String payload) async {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        content: Text("Notification : $payload"),
+      ),
+    );
+  }
+
+
+
+ShowNotification()async{
+
+
+  const AndroidNotificationDetails androidNotificationDetails =
+    AndroidNotificationDetails('your channel id', 'your channel name',
+        channelDescription: 'your channel description',
+        importance: Importance.max,
+        priority: Priority.high,
+        ticker: 'ticker');
+const NotificationDetails notificationDetails =
+    NotificationDetails(android: androidNotificationDetails);
+await flutterLocalNotificationsPlugin!.show(
+    0, 'plain title', 'plain body', notificationDetails,
+    payload: 'item x');
+}
+
+
+
+
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -52,9 +136,12 @@ class _MyAppState extends State<MyApp> {
             ),
           )
         ],
-        title: const Text(
-          'Alarm Clock ',
-          style: TextStyle(color: Colors.white),
+        title: GestureDetector(
+          onTap: ShowNotification,
+          child: const Text(
+            'Alarm Clock ',
+            style: TextStyle(color: Colors.white),
+          ),
         ),
         centerTitle: true,
       ),
@@ -131,12 +218,7 @@ class _MyAppState extends State<MyApp> {
                                         }),
                                   ],
                                 ),
-                                Row(
-                                  children: [
-                                    Text(alarm.modelist[index].when!),
-                                    Icon(Icons.delete),
-                                  ],
-                                )
+                                Text(alarm.modelist[index].when!)
                               ],
                             ),
                           ),
